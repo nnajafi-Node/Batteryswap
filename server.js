a8d109b9aa8289a33b12266f27cd1c6c47c16faf
nnajafi-Node <76422010+nnajafi-Node@users.noreply.github.com>
2020-12-21 10:59:14 +0330

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const body_parser = require('body-parser');
+const passport = require('passport');
+
+const app = express()
+const cors = require('cors')
+app.use(cors())
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+app.use(body_parser.urlencoded());
+app.use(body_parser.json());
+ 
+
+const profileApi = require('./routesBS/apiBS/profile');
+const registerApi = require('./routesBS/apiBS/register');
+const batteryApi = require('./routesBS/apiBS/battery');
+
+app.use('/profile',profileApi);
+app.use('/register',registerApi);
+app.use('/battery',batteryApi);
+
+
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.listen(5000, () => {
+    console.log(`Server is Running on port 5000`)
+  })
